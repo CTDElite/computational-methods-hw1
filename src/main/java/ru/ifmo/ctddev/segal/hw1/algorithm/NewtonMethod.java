b2d9da7a8c69fdf278f282270ead86d1674be0c8
Dmitry Tomp <dmitrytomp@gmail.com>
2015-11-18 21:21:57 +0300

    Add interfaces for task 1

--- src/main/java/ru/ifmo/ctddev/segal/hw1/algorithm/NewtonMethod.java
+++ src/main/java/ru/ifmo/ctddev/segal/hw1/algorithm/NewtonMethod.java
@@ -2,7 +2,7 @@ package ru.ifmo.ctddev.segal.hw1.algorithm;
 
 import org.apache.commons.math3.complex.Complex;
 import org.apache.commons.math3.util.Pair;
-import ru.ifmo.ctddev.segal.hw1.model.FunctionWithDerivative;
+import ru.ifmo.ctddev.segal.hw1.model.ComplexDifferentiableFunction;
 
 import java.util.List;
 
@@ -10,6 +10,6 @@ import java.util.List;
  * @author Daniyar Itegulov
  */
 public interface NewtonMethod {
-    Pair<Complex, Integer> getRoot(FunctionWithDerivative function, Complex begin);
-    List<Complex> getPath(FunctionWithDerivative functionWithDerivative, Complex begin);
+    Pair<Complex, Integer> getRoot(ComplexDifferentiableFunction function, Complex begin);
+    List<Complex> getPath(ComplexDifferentiableFunction complexDifferentiableFunction, Complex begin);
 }
