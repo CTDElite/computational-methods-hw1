b040e65d3f7e92556990c2b16e73686b2de9557e
alex-700 <alex_700_95@mail.ru>
2015-11-18 17:10:45 +0300

    change end condition on Newton

--- src/main/java/ru/ifmo/ctddev/segal/hw1/algorithm/NewtonMethodImpl.java
+++ src/main/java/ru/ifmo/ctddev/segal/hw1/algorithm/NewtonMethodImpl.java
@@ -3,8 +3,13 @@ package ru.ifmo.ctddev.segal.hw1.algorithm;
 import org.apache.commons.math3.complex.Complex;
 import ru.ifmo.ctddev.segal.hw1.model.FunctionWithDerivative;
 
+
+/**
+ * @author Aleksei Latyshev
+ */
+
 public class NewtonMethodImpl implements NewtonMethod {
-    public static final double EPS = 1e-9;
+    public static final double EPS = 1e-16;
     public static final int COUNT = 1_000;
 
     @Override
@@ -12,12 +17,11 @@ public class NewtonMethodImpl implements NewtonMethod {
         Complex current = begin;
         boolean found = false;
         for (int it = 0; it < COUNT; it++) {
-            Complex next = current.subtract(function.getValue(current).divide(function.getDerivativeValue(current)));
-            if (next.subtract(current).abs() < EPS) {
+            current = current.subtract(function.getValue(current).divide(function.getDerivativeValue(current)));
+            if (function.getValue(current).abs() < EPS) {
                 found = true;
                 break;
             }
-            current = next;
         }
         if (found) {
             return current;
